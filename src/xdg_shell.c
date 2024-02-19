f7d62a7ce7410dc08166c11c371c4b05e0849823
Antar Azri <azantar@proton.me>
2024-02-19 02:42:58 +0100

    add handlers

--- src/xdg_shell.c
+++ src/xdg_shell.c
@@ -1,29 +1,96 @@
 #include "xdg_shell.h"
 
+static void toplevel_map(struct wl_listener *listener, void *data) {}
+
+static void toplevel_unmap(struct wl_listener *listener, void *data) {}
+
+static void toplevel_commit(struct wl_listener *listener, void *data) {}
+
+static void toplevel_destroy(struct wl_listener *listener, void *data) {
+  struct k_toplevel *toplevel = wl_container_of(listener, toplevel, destroy);
+
+  wl_list_remove(&toplevel->map.link);
+  wl_list_remove(&toplevel->unmap.link);
+  wl_list_remove(&toplevel->commit.link);
+  wl_list_remove(&toplevel->destroy.link);
+
+  wl_list_remove(&toplevel->request_fullscreen.link);
+  wl_list_remove(&toplevel->request_minimize.link);
+  wl_list_remove(&toplevel->request_maximize.link);
+  wl_list_remove(&toplevel->request_move.link);
+  wl_list_remove(&toplevel->request_resize.link);
+
+  wl_list_remove(&toplevel->link);
+  free(toplevel);
+}
+
+static void toplevel_request_fullscreen(struct wl_listener *listener,
+                                        void *data) {}
+
+static void toplevel_request_maximize(struct wl_listener *listener,
+                                      void *data) {}
+
+static void toplevel_request_minimize(struct wl_listener *listener,
+                                      void *data) {}
+
+static void toplevel_request_move(struct wl_listener *listener, void *data) {}
+
+static void toplevel_request_resize(struct wl_listener *listener, void *data) {}
+
 void new_xdg_toplevel(struct wl_listener *listener, void *data) {
-	struct k_state *state = wl_container_of(listener, state, new_xdg_surface);
-	struct wlr_xdg_toplevel *xdg_toplevel = data;
+  struct k_state *state = wl_container_of(listener, state, new_xdg_surface);
+  struct wlr_xdg_toplevel *xdg_toplevel = data;
 
-	struct k_toplevel *toplevel = calloc(1, sizeof(struct k_toplevel));
-	toplevel->state = state;
-	toplevel->xdg_toplevel = xdg_toplevel;
+  struct k_toplevel *toplevel = calloc(1, sizeof(struct k_toplevel));
+  toplevel->state = state;
+  toplevel->xdg_toplevel = xdg_toplevel;
 
-	wl_list_insert(&toplevel->state->toplevels, &toplevel->link);
+  // listen to toplevel events
+  toplevel->map.notify = toplevel_map;
+  wl_signal_add(&xdg_toplevel->base->surface->events.map, &toplevel->map);
+  toplevel->unmap.notify = toplevel_unmap;
+  wl_signal_add(&xdg_toplevel->base->surface->events.unmap, &toplevel->unmap);
+  toplevel->commit.notify = toplevel_commit;
+  wl_signal_add(&xdg_toplevel->base->surface->events.commit, &toplevel->commit);
+  toplevel->destroy.notify = toplevel_destroy;
+  wl_signal_add(&xdg_toplevel->base->events.destroy, &toplevel->destroy);
+
+  toplevel->scene_tree = wlr_scene_xdg_surface_create(
+      &toplevel->state->scene->tree, xdg_toplevel->base);
+  toplevel->scene_tree->node.data = toplevel;
+  xdg_toplevel->base->data = toplevel->scene_tree;
+
+  toplevel->request_fullscreen.notify = toplevel_request_fullscreen;
+  wl_signal_add(&xdg_toplevel->events.request_fullscreen,
+                &toplevel->request_fullscreen);
+  toplevel->request_maximize.notify = toplevel_request_maximize;
+  wl_signal_add(&xdg_toplevel->events.request_maximize,
+                &toplevel->request_maximize);
+  toplevel->request_minimize.notify = toplevel_request_minimize;
+  wl_signal_add(&xdg_toplevel->events.request_minimize,
+                &toplevel->request_minimize);
+  toplevel->request_move.notify = toplevel_request_move;
+  wl_signal_add(&xdg_toplevel->events.request_move, &toplevel->request_move);
+  toplevel->request_resize.notify = toplevel_request_resize;
+  wl_signal_add(&xdg_toplevel->events.request_resize,
+                &toplevel->request_resize);
+
+  wl_list_insert(&toplevel->state->toplevels, &toplevel->link);
 }
 
 void new_xdg_surface(struct wl_listener *listener, void *data) {
-	struct wlr_xdg_surface *xdg_surface = data;
+  struct wlr_xdg_surface *xdg_surface = data;
 
-	if (xdg_surface->role == WLR_XDG_SURFACE_ROLE_TOPLEVEL) {
-		new_xdg_toplevel(listener, xdg_surface->toplevel);
-	}
+  if (xdg_surface->role == WLR_XDG_SURFACE_ROLE_TOPLEVEL) {
+    new_xdg_toplevel(listener, xdg_surface->toplevel);
+  }
 }
 
 void init_xdg_shell(struct k_state *state) {
-	// create XDG SHELL version 3
-	state->xdg_shell = wlr_xdg_shell_create(state->display, 3);
+  // create XDG SHELL version 3
+  state->xdg_shell = wlr_xdg_shell_create(state->display, 3);
 
-	// TODO: support different WLR versions
-	state->new_xdg_surface.notify = new_xdg_surface;
-	wl_signal_add(&state->xdg_shell->events.new_surface, &state->new_xdg_surface);
+  // TODO: support different WLR versions
+  state->new_xdg_surface.notify = new_xdg_surface;
+  wl_signal_add(&state->xdg_shell->events.new_surface, &state->new_xdg_surface);
 }
--- src/xdg_shell.h
+++ src/xdg_shell.h
@@ -4,27 +4,27 @@
 #include "state.h"
 
 struct k_toplevel {
-	struct k_state *state;
-	struct wlr_xdg_toplevel *xdg_toplevel;
-	struct wlr_scene_tree *scene_tree;
+  struct k_state *state;
+  struct wlr_xdg_toplevel *xdg_toplevel;
+  struct wlr_scene_tree *scene_tree;
 
-	// listeners for toplevel events
-	struct wl_listener map;
-	struct wl_listener unmap;
-	struct wl_listener commit;
-	struct wl_listener destroy;
-	struct wl_listener new_popup;
-	struct wl_listener request_fullscreen;
-	struct wl_listener request_maximize;
-	struct wl_listener request_minimize;
-	struct wl_listener request_move;
-	struct wl_listener request_resize;
+  // listeners for toplevel events
+  struct wl_listener map;
+  struct wl_listener unmap;
+  struct wl_listener commit;
+  struct wl_listener destroy;
+  struct wl_listener new_popup;
+  struct wl_listener request_fullscreen;
+  struct wl_listener request_maximize;
+  struct wl_listener request_minimize;
+  struct wl_listener request_move;
+  struct wl_listener request_resize;
 
-	// geometry of the toplevel
-	struct wlr_box geometry;
+  // geometry of the toplevel
+  struct wlr_box geometry;
 
-	// link to next toplevel node
-	struct wl_list link;
+  // list node
+  struct wl_list link;
 };
 
 void init_xdg_shell(struct k_state *state);
