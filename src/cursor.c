1e040d88f6600531ae9ff6aa49730e0dfce9aa25
Antar Azri <azantar@proton.me>
2024-02-19 15:44:52 +0100

    find toplevel

--- src/cursor.c
+++ src/cursor.c
@@ -1,5 +1,28 @@
 #include "cursor.h"
 
+struct k_toplevel *toplevel_at(struct k_state *state, double lx, double ly,
+                               struct wlr_surface **surface, double *sx,
+                               double *sy) {
+  struct wlr_scene_node *node =
+      wlr_scene_node_at(&state->scene->tree.node, lx, ly, sx, sy);
+  if (node == NULL || node->type != WLR_SCENE_NODE_BUFFER) {
+    return NULL;
+  }
+  struct wlr_scene_buffer *scene_buffer = wlr_scene_buffer_from_node(node);
+  struct wlr_scene_surface *scene_surface =
+      wlr_scene_surface_try_from_buffer(scene_buffer);
+  if (!scene_surface) {
+    return NULL;
+  }
+
+  *surface = scene_surface->surface;
+  struct wlr_scene_tree *tree = node->parent;
+  while (tree != NULL && tree->node.data == NULL) {
+    tree = tree->node.parent;
+  }
+  return tree->node.data;
+}
+
 static void process_cursor_motion(struct k_state *state, uint32_t time) {
   if (state->cursor->cursor_mode == K_CURSOR_MOVE) {
     return;
@@ -10,11 +33,14 @@ static void process_cursor_motion(struct k_state *state, uint32_t time) {
   double sx, sy;
   struct wlr_seat *seat = state->seat;
   struct wlr_surface *surface = NULL;
+  struct k_toplevel *toplevel =
+      toplevel_at(state, state->cursor->wlr_cursor->x,
+                  state->cursor->wlr_cursor->y, &surface, &sx, &sy);
 
-  // find toplevel
-
-  wlr_cursor_set_xcursor(state->cursor->wlr_cursor,
-                         state->cursor->xcursor_manager, "default");
+  if (!toplevel) {
+    wlr_cursor_set_xcursor(state->cursor->wlr_cursor,
+                           state->cursor->xcursor_manager, "default");
+  }
 
   if (surface) {
     wlr_seat_pointer_notify_enter(seat, surface, sx, sy);
@@ -25,7 +51,7 @@ static void process_cursor_motion(struct k_state *state, uint32_t time) {
 }
 
 static void cursor_motion_absolute(struct wl_listener *listener, void *data) {
-  struct k_cursor *cursor = wl_container_of(listener, cursor, request_cursor);
+  struct k_cursor *cursor = wl_container_of(listener, cursor, motion_absolute);
   struct wlr_pointer_motion_absolute_event *event = data;
   wlr_cursor_warp_absolute(cursor->wlr_cursor, &event->pointer->base, event->x,
                            event->y);
@@ -34,7 +60,7 @@ static void cursor_motion_absolute(struct wl_listener *listener, void *data) {
 }
 
 static void cursor_motion(struct wl_listener *listener, void *data) {
-  struct k_cursor *cursor = wl_container_of(listener, cursor, request_cursor);
+  struct k_cursor *cursor = wl_container_of(listener, cursor, motion);
   struct wlr_pointer_motion_event *event = data;
   wlr_cursor_move(cursor->wlr_cursor, &event->pointer->base, event->delta_x,
                   event->delta_y);
@@ -43,7 +69,7 @@ static void cursor_motion(struct wl_listener *listener, void *data) {
 }
 
 static void cursor_axis(struct wl_listener *listener, void *data) {
-  struct k_cursor *cursor = wl_container_of(listener, cursor, request_cursor);
+  struct k_cursor *cursor = wl_container_of(listener, cursor, axis);
   struct wlr_pointer_axis_event *event = data;
   wlr_seat_pointer_notify_axis(cursor->state->seat, event->time_msec,
                                event->orientation, event->delta,
@@ -51,7 +77,7 @@ static void cursor_axis(struct wl_listener *listener, void *data) {
 }
 
 static void cursor_frame(struct wl_listener *listener, void *data) {
-  struct k_cursor *cursor = wl_container_of(listener, cursor, request_cursor);
+  struct k_cursor *cursor = wl_container_of(listener, cursor, frame);
   wlr_seat_pointer_notify_frame(cursor->state->seat);
 }
 
@@ -68,7 +94,7 @@ static void request_cursor(struct wl_listener *listener, void *data) {
   }
 }
 
-void init_cursor(struct k_state *state) {
+void init_cursor(struct k_state *state, struct wlr_input_device *device) {
   struct k_cursor *cursor = calloc(1, sizeof(struct k_cursor));
   cursor->wlr_cursor = wlr_cursor_create();
   cursor->cursor_mode = K_CURSOR_PASSTHROUGH;
@@ -89,7 +115,7 @@ void init_cursor(struct k_state *state) {
   cursor->motion.notify = cursor_motion;
   wl_signal_add(&cursor->wlr_cursor->events.motion, &cursor->motion);
 
-  cursor->motion.notify = cursor_motion_absolute;
+  cursor->motion_absolute.notify = cursor_motion_absolute;
   wl_signal_add(&cursor->wlr_cursor->events.motion_absolute,
                 &cursor->motion_absolute);
 
@@ -98,6 +124,7 @@ void init_cursor(struct k_state *state) {
                 &cursor->request_cursor);
 
   wlr_cursor_attach_output_layout(cursor->wlr_cursor, state->output_layout);
+  wlr_cursor_attach_input_device(cursor->wlr_cursor, device);
 }
 
 void destroy_cursor(struct k_cursor *cursor) {
