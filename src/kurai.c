4a555bf818a8e03945cf9ee173d924a88de659fd
Antar Azri <azantar@proton.me>
2024-02-19 02:28:55 +0100

    add cmd startup option

--- src/kurai.c
+++ src/kurai.c
@@ -15,10 +15,26 @@ void termination_handler(int signum) {
   }
 }
 
-int main() {
+int main(int argc, char **argv) {
   wlr_log_init(WLR_DEBUG, NULL);
-
   char *startup_cmd = NULL;
+
+  int c;
+  while ((c = getopt(argc, argv, "s:h")) != -1) {
+    switch (c) {
+    case 's':
+      startup_cmd = optarg;
+      break;
+    default:
+      printf("Usage: %s [-s startup command]\n", argv[0]);
+      return 0;
+    }
+  }
+  if (optind < argc) {
+    printf("Usage: %s [-s startup command]\n", argv[0]);
+    return 0;
+  }
+
 
   if (!init_state(&state)) {
     wlr_log(WLR_ERROR, "Failed to initialize state");
