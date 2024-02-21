dfa36a1d0d47532f518744b73d07b6d982aa17f2
Antar Azri <azantar@proton.me>
2024-02-21 02:10:18 +0100

    remove unecessary new lines

--- src/input.c
+++ src/input.c
@@ -7,17 +7,17 @@ void new_input(struct wl_listener *listener, void *data) {
   struct wlr_input_device *device = data;
   switch (device->type) {
   case WLR_INPUT_DEVICE_KEYBOARD:
-    wlr_log(WLR_INFO, "New keyboard\n");
+    wlr_log(WLR_INFO, "New keyboard");
     // handle keyboard device
     init_keyboard(state, device);
     break;
   case WLR_INPUT_DEVICE_POINTER:
-    wlr_log(WLR_INFO, "New pointer\n");
+    wlr_log(WLR_INFO, "New pointer");
     // handle pointer device
     init_cursor(state, device);
     break;
   default:
-    wlr_log(WLR_INFO, "New unsupported device\n");
+    wlr_log(WLR_INFO, "New unsupported device");
   }
 
   uint32_t caps = WL_SEAT_CAPABILITY_POINTER;
